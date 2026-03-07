b9e9c272226d32ed3c901f2a1303902d0d32daec
Dory247 <yenpham18082006@gmail.com>
2026-03-08 01:06:34 +0700

    cập nhập sql

--- sql/migration_add_ungvien_status_da_chuyen_nhan_vien.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add candidate status after onboarding to employee
--- Apply on existing databases (MySQL 8+).
-ALTER TABLE UNGVIEN
-MODIFY COLUMN trangThai ENUM('moi', 'dang_phong_van', 'trung_tuyen', 'da_chuyen_nhan_vien', 'tu_choi')
-DEFAULT 'moi';
-
